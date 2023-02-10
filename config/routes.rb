18fd2674a089e9f28ea9d44e96c30906928addcc
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-02-10 23:48:20 +0000

    [Update]コントローラ作成_2023-0211-0848

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'staffs/index'
+    get 'staffs/show'
+    get 'staffs/edit'
+    get 'staffs/search'
+  end
+  namespace :admin do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :admins
   devise_for :staffs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
