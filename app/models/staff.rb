7348968eb297a4c2a0a5b3e30b01d888e4532ecd
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-02-10 12:42:49 +0000

    [Update]ActiveStorageの導入_staffモデル_2023-0210-2142

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,13 +2,22 @@ class Staff < ApplicationRecord
   has_many :departments
   has_many :positions
   has_many :admins
-  
+
   #社員の勤務形態のenum記述
   enum work_style: { retirement: 0, work: 1, short_work: 2, leave_of_absence: 3 }
-  
+
   #staffモデルにimage（プロフィール画像)を持たせる記述
   has_one_attached :image
-  
+
+  #プロフィール画像の定義
+  def get_profile_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [width, height]).processed
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
