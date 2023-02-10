ebb3ff3704c9de267fefb7f3bc46b820e090679f
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-02-10 11:53:57 +0000

    [Update]devise_admin_2023-0210-2053

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_10_112259) do
+ActiveRecord::Schema.define(version: 2023_02_10_114803) do
+
+  create_table "admins", force: :cascade do |t|
+    t.integer "staff_id", null: false
+    t.boolean "is_active", default: false, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "staffs", force: :cascade do |t|
     t.string "last_name", null: false
