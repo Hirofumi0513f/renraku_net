419823a969e4c9e812a720c718195834b5d478bd
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-02-10 12:08:40 +0000

    [Update]make_model_department_2023-0210-2108

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_10_114803) do
+ActiveRecord::Schema.define(version: 2023_02_10_120612) do
 
   create_table "admins", force: :cascade do |t|
     t.integer "staff_id", null: false
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2023_02_10_114803) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.integer "staff_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "staffs", force: :cascade do |t|
