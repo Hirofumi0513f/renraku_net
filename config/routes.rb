5f6aca778445a6dd7aa6db7b38fb00e060946f1c
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-02-11 11:41:23 +0000

    [Update]staffコントローラのsearchに関する名前付きルーティングの追記_2023-0211-2041

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'positions/index'
-    get 'positions/create'
-    get 'positions/edit'
-    get 'positions/update'
-  end
     devise_for :admins, controllers: {
     sessions: "admin/controllers"
   }
@@ -20,17 +14,23 @@ Rails.application.routes.draw do
 
   namespace :public do
     resources :staffs, only: [:index, :show, :edit, :update, :search]
+    #社員検索用の名前付きルーティング
+    get '/staffs/search', to:"staff#search", as: "search"
   end
 
   namespace :admin do
     root to: "homes#top"
     resources :acitives, only: [:update]
 
-    resources :staffs, only: [:index, :show, :edit, :search]
+    resources :staffs, only: [:index, :show, :edit]
+    #社員検索用の名前付きルーティング
+    get '/staffs/search', to:"staffs#search", as: "admin_search"
 
     resources :departments, only: [:index, :create, :edit, :update]
 
     resources :divisions, only: [:index, :create, :edit, :update]
+
+    resources :positions, only: [:index, :create, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
