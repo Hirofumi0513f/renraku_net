e53f8e426cbdf2fba0b4ae95f4068852c71b86d9
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-02-18 07:32:25 +0000

    [Update]staffコントローラ_2023-0218-1632

--- app/controllers/public/staffs_controller.rb
+++ app/controllers/public/staffs_controller.rb
@@ -1,16 +1,37 @@
 class Public::StaffsController < ApplicationController
   def index
+    # 社員IDごとに10件ずつ表示させる
+    @staffs = Staff.order(:id).page(params[:page]).per(10)
   end
 
   def show
+    # 社員IDに紐づく詳細情報を表示させる
+    @staff =Staff.find(params[:id])
   end
 
   def edit
+    @staff =Staff.find(params[:id])
   end
 
   def update
+    @staff = Staff.find(params[:id])
+    if @staff.update(staff_params)
+      flash[:notice] ="社員情報が更新されました"
+      # 社員情報の詳細ページに飛ばす
+      redirect_to public_staff_path(@staff.id)
+    else
+      render :edit
+    end
   end
 
   def search
   end
+
+  # 以下ストロングパラメータ（意図しない社員データの登録・更新を防ぐ）
+  private
+    def staff_params
+      params.require(:staff).permit(
+        :last_name, :first_name, :last_name_kana, :first_name_kana,
+        :telephone_number, :email, :work_style, :is_admin)
+    end
 end
