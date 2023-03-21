e2c1aeaf541d79a1adc90efb4a5067ff52ef6078
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-03-21 12:31:22 +0000

    [Update]department画面_エラーメッセージ表示機能_2023-0321-2131

--- app/controllers/public/departments_controller.rb
+++ app/controllers/public/departments_controller.rb
@@ -14,10 +14,12 @@ class Public::DepartmentsController < ApplicationController
   def create
     @department = Department.new(department_params)
     if @department.save
-      flash[:notice] = "部名が追加されました。"
+      flash[:notice] = "部名が追加されました"
       # 登録に成功したら、部署一覧ページに飛ばす
       redirect_to public_departments_path
     else
+      flash[:notice] = "部名の追加に失敗しました"
+      @departments = Department.order(:id).page(params[:page]).per(9)
       # 部署一覧ページに部署追加画面を作成するため、同じく部署一覧ページに飛ばす
       render :index
     end
@@ -29,9 +31,9 @@ class Public::DepartmentsController < ApplicationController
 
   def update
     @departments = Department.find(params[:id])
-    if @departments.update!(department_params)
+    if @departments.update(department_params)
       # 部署情報の更新が成功したら部署一覧ページに遷移させる
-      flash[:notice] = "部署情報が更新されました"
+      flash[:notice] = "部名が更新されました"
       redirect_to public_departments_path
     else
       # 部署更新が失敗したら部署情報編集ページに戻す
