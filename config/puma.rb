542bea2e1cad1f8992a308b44b0b431890fd4cba
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2023-03-27 12:26:33 +0000

    [Update]config/puma.rb_2023-0327-2126

--- config/puma.rb
+++ config/puma.rb
@@ -53,6 +53,6 @@ if Rails.env.production?
     File.join(rails_root, 'log', 'puma-error.log'),
     true
   )
-  # デーモン
+  # デーモン（Daemon):アプリケーションを起動する時に、バックグラウンドでアプリケーションサーバーを常時起動させておくために設定するもの
   daemonize
 end
